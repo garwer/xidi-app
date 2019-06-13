b9a6375665cab04ef37b729ff6375300836fcbc0
linjiawei <linjw@ffcs.cn>
2019-06-13 18:10:47 +0800

    调整

--- monitor-center/src/test/java/com/garwer/monitor/MonitorCenterApplicationTests.java
+++ monitor-center/src/test/java/com/garwer/monitor/MonitorCenterApplicationTests.java
@@ -1,4 +1,4 @@
-package com.garwer.eureka;
+package com.garwer.monitor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NacosApplicationTests {
+public class MonitorCenterApplicationTests {
     @Value("${garwer}")
 
     private String garwer;
